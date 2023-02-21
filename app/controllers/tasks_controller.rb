c31c9375b2f58891a5761fc76f789eb820b463ae
Aslihan Hird <aslihan.hird@gmail.com>
2023-02-21 14:24:53 +0100

    got the delete icon link working

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,10 @@
 class TasksController < ApplicationController
+  before_action :find_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -18,18 +18,15 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    task = Task.find(params[:id])
-    task.update(edit_params)
-    redirect_to task_path(task)
+    @task.update(edit_params)
+    redirect_to task_path(@task)
   end
 
   def destroy
-    task = Task.find(params[:id])
-    task.destroy
+    @task.destroy
 
     redirect_to tasks_path, status: :see_other
   end
@@ -42,5 +39,9 @@ class TasksController < ApplicationController
 
   def edit_params
     params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def find_task
+    @task = Task.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get 'tasks', to: 'tasks#index'
-  get 'tasks/new', to: 'tasks#new'
-  get 'tasks/:id', to: 'tasks#show', as: 'task'
-  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  # get 'tasks', to: 'tasks#index'
+  # get 'tasks/new', to: 'tasks#new'
+  # get 'tasks/:id', to: 'tasks#show', as: 'task'
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
 
-  post 'tasks', to: 'tasks#create'
-  patch 'tasks/:id', to: 'tasks#update'
-  delete 'tasks/:id', to: 'tasks#destroy'
+  # post 'tasks', to: 'tasks#create'
+  # patch 'tasks/:id', to: 'tasks#update'
+  # delete 'tasks/:id', to: 'tasks#destroy'
+  resources :tasks
 end
